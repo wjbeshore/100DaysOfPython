e1aba66dfc07fce04797ea435f23357ad1d24219
will <wjbeshore@gmail.com>
2021-08-30 17:58:57 -0500

    Finished spirograph

--- Turtles/main.py
+++ Turtles/main.py
@@ -1,17 +1,22 @@
 import turtle as t
 import random
 timmy = t.Turtle()
-t.colormode(255
-                )
+t.colormode(255)
+
 def random_color():
     r = random.randint(0,255)
     g = random.randint(0, 255)
     b = random.randint(0, 255)
-    return (r, g, b)
+    color = (r, g, b)
+    print(r + g + b)
+    return color
 
 timmy.speed("fastest")
 
-timmy.circle(100)
+for i in range(50):
+    timmy.color(random_color())
+    timmy.circle(100)
+    timmy.setheading(timmy.heading()+10)
 
 screen = t.Screen()
 screen.exitonclick()
