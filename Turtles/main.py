8a2484bee30bf39fbad8159144e94dcd17bbed93
will <wjbeshore@gmail.com>
2021-08-31 17:44:07 -0500

    Added comments

--- Turtles/main.py
+++ Turtles/main.py
@@ -3,6 +3,7 @@ import random
 timmy = t.Turtle()
 t.colormode(255)
 
+# Creates random colors
 def random_color():
     r = random.randint(0,255)
     g = random.randint(0, 255)
@@ -10,10 +11,11 @@ def random_color():
     color = (r, g, b)
     print(r + g + b)
     return color
-
+# Changes Speed
 timmy.speed("fastest")
 
-for i in range(50):
+# Creates circle then moves heading to create spirograph
+for i in range(40):
     timmy.color(random_color())
     timmy.circle(100)
     timmy.setheading(timmy.heading()+10)
